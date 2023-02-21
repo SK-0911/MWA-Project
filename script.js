90d2c359e2eea09f5e5ee8e2311d9b25afd5435f
Shubham Kumar <shubham.911k@gmail.com>
2023-02-21 21:41:26 +0530

    Progress of form added and some basic animations

--- script.js
+++ script.js
@@ -20,3 +20,16 @@ function updateText(){
     }
     setTimeout(updateText, 300)
 }
+
+/***** Multi - step form in register.html *****/
+
+var form1 = document.getElementById("form1")
+var form2 = document.getElementById("form2")
+
+var next1 = document.getElementById("next1")
+var back1 = document.getElementById("back1")
+var next2 = document.getElementById("next2")
+
+next1.onclick = function(){
+    
+}
